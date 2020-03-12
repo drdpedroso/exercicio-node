92f06644c8383f7d6cf45834a0f036baef7de7a6
Eduardo <drdpedroso@gmail.com>
2020-03-12 17:19:41 -0300

    ss

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.use('public', express.static('public'))
-
-app.get('/', function (req, res) {
-    res.send('Hello World!')
-})
+app.use(express.static('public'))
 
 app.listen(3000, function () {
     console.log('Ouvindo a porta 3000!')
